9d3a59d733052b4f3eb1917e463d24cd98dc548f
hanif ryanas <hanifryanas@gmail.com>
2022-07-28 22:03:27 +0700

    add filter controller

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -3,6 +3,8 @@ const ProductController = require('../controllers/product.controller');
 const uploadImage = require('../middleware/uploadImage');
 
 router.get('/', ProductController.getAllProducts);
+// add get with specified data filter 
+router.get('/:type', ProductController.getProductByType);
 router.post('/', uploadImage.single('image'), ProductController.createProduct);
 
 module.exports = router;
